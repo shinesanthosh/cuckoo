018315609d7d48d40df2657b377582a06fd11e89
shinesanthosh <shinesanthosh2000@gmail.com>
2021-05-17 13:56:59 +0530

    updated to print formatted thread with a username

--- main.py
+++ main.py
@@ -10,28 +10,35 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
-def check_mentions(api, keywords, since_id):
+def check_mentions(api, twitter_handle, since_id):
     logger.info("Retrieving mentions")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
         new_since_id = max(tweet.id, new_since_id)
-        if tweet.in_reply_to_status_id is not None:
+
+        # if tweet.in_reply_to_status_id is not None:
+        #     continue
+        if tweet.user.screen_name != twitter_handle:
             continue
-        print(
-            f"{tweet.user.screen_name} mwntioned you in {tweet.text} The thread is following \n"
-        )
-        print(getThread(tweet.id))
+
+        print(f"{tweet.user.screen_name} mwntioned you in {tweet.text}")
+        thread = getThread(tweet.id)
+        if thread["meta"]["result_count"] == 0:
+            print("Can't find a thread associated with this tweet\n---------------\n")
+            continue
+
+        print("\n\nThe thread is following: \n")
+        for i in thread["data"]:
+            print(f"{i['text']}\n\n")
 
     return new_since_id
 
 
 def main():
+    t_handle = ""
     api = create_api()
     since_id = 1
-    while True:
-        since_id = check_mentions(api, ["how's"], since_id)
-        logger.info("Waiting...")
-        time.sleep(60)
+    since_id = check_mentions(api, t_handle, since_id)
 
 
 if __name__ == "__main__":
