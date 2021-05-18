143fe80dd2264e646000263efcdb788be6f08a22
shinesanthosh <shinesanthosh2000@gmail.com>
2021-05-19 01:45:47 +0530

    importable  module & write to text file

--- thread_fetch.py
+++ thread_fetch.py
@@ -1,6 +1,6 @@
 import tweepy
 import logging
-import time
+
 
 from config import create_api
 from conversation import getThread
@@ -18,6 +18,7 @@ def search_user(users, id):
 
 
 def check_mentions(api, twitter_handle, since_id):
+    f = open("output.txt","w")
     logger.info("Retrieving mentions")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
@@ -28,18 +29,30 @@ def check_mentions(api, twitter_handle, since_id):
         if tweet.user.screen_name != twitter_handle:
             continue
 
-        print(f"{tweet.user.screen_name} mwntioned you in {tweet.text}")
+        # print(f"{tweet.user.screen_name} mentioned the bot: {tweet.text}")
+        f.write(f"->{tweet.user.screen_name} mentioned the bot: {tweet.text}")
         thread = getThread(tweet.id)
         if thread["meta"]["result_count"] == 0:
-            print("Can't find a thread associated with this tweet\n---------------\n")
+            # print("Can't find a thread associated with this tweet\n---------------\n")
+            f.write("-->Can't find a thread associated with this tweet\n---------------\n")
             continue
 
-        print("\n\nThe thread is following: \n")
+        # print("\n\nThe thread is following: \n")
+        f.write("\n\nThe thread is following: \n")
         for i in thread["data"]:
             un = search_user(thread["includes"]["users"], i["author_id"])
-            print(f"{un} : {i['text']}\n\n")
+            # print(f"{un} : {i['text']}\n\n")
+            f.write(f"-->{un} : {i['text']}\n\n")
+        # print("\n---------------\n")
+        f.write("\n---------------\n")
 
+    f.close()
     return new_since_id
+
+def writeThread(t_handle):
+    api = create_api()
+    since_id = 1
+    since_id = check_mentions(api, t_handle, since_id)
 
 
 def main():
